fc5fe0082453828a06a230d9e326bbec49362ab3
Sachin302cse <sachin.19cs302@citranchi.ac.in>
2025-02-19 16:08:47 +0530

    project initiated

---
+++ src/test/java/com/example/dine_in_order_api/DineInOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dine_in_order_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DineInOrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
