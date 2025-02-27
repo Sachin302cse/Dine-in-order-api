0421d13f834affd84edf29dcfcd9a2df68039a3f
Sachin302cse <sachin.19cs302@citranchi.ac.in>
2025-02-27 17:39:04 +0530

    adding userRepositry

--- src/main/java/com/example/dine_in_order_api/repositry/UserRepositry.java
+++ src/main/java/com/example/dine_in_order_api/repositry/UserRepositry.java
@@ -1,4 +1,9 @@
 package com.example.dine_in_order_api.repositry;
 
-public interface UserRepositry {
+import com.example.dine_in_order_api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepositry extends JpaRepository<User, Long>{
 }
