c89df828248b05f16b27ce933e3ad2ee065546d8
Sachin302cse <sachin.19cs302@citranchi.ac.in>
2025-02-27 17:36:21 +0530

    adding usercontroller

--- src/main/java/com/example/dine_in_order_api/controller/UserController.java
+++ src/main/java/com/example/dine_in_order_api/controller/UserController.java
@@ -1,4 +1,29 @@
 package com.example.dine_in_order_api.controller;
 
+import com.example.dine_in_order_api.model.User;
+import com.example.dine_in_order_api.service.UserService;
+import com.example.dine_in_order_api.utility.ResponseBuilder;
+import com.example.dine_in_order_api.utility.ReponseStructure;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+@AllArgsConstructor
 public class UserController {
+
+    @Autowired
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<ReponseStructure<User>> registerUser(@RequestBody User user)
+    {
+        user = userService.registerUser(user);
+        return ResponseBuilder.success(HttpStatus.CREATED, "User created" , user);
+
+    }
 }
