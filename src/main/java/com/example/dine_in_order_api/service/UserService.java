c999683331d78f7affd41e62aa605d7f56b6d6e5
Sachin302cse <sachin.19cs302@citranchi.ac.in>
2025-02-27 17:39:54 +0530

    adding userservice

--- src/main/java/com/example/dine_in_order_api/service/UserService.java
+++ src/main/java/com/example/dine_in_order_api/service/UserService.java
@@ -1,4 +1,10 @@
 package com.example.dine_in_order_api.service;
 
+import com.example.dine_in_order_api.model.User;
+
 public interface UserService {
+
+    public User registerUser(User user);
+
+
 }
--- src/main/java/com/example/dine_in_order_api/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/dine_in_order_api/service/impl/UserServiceImpl.java
@@ -1,4 +1,49 @@
 package com.example.dine_in_order_api.service.impl;
 
-public class UserServiceImpl {
+import com.example.dine_in_order_api.enums.UserRole;
+import com.example.dine_in_order_api.model.Admin;
+import com.example.dine_in_order_api.model.Staff;
+import com.example.dine_in_order_api.model.User;
+import com.example.dine_in_order_api.repositry.UserRepositry;
+import com.example.dine_in_order_api.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private final UserRepositry userRepositry;
+
+    @Override
+    public User registerUser(User user) {
+        User user2 = this.createUserByRole(user.getUserRole());
+        this.mapToNewUser(user,user2);
+        return userRepositry.save(user2);
+    }
+
+    private User createUserByRole(UserRole role){
+        User user;
+
+        switch(role){
+            case ADMIN-> user = new Admin();
+            case STAFF-> user = new Staff();
+            default->throw new RuntimeException("Failed to register user, invalid user type");
+        }
+        return user;
+    }
+
+    private void mapToNewUser(User user , User newUser){
+        newUser.setUsername(user.getUsername());
+        newUser.setEmail(user.getEmail());
+        newUser.setPassword(user.getPassword());
+        newUser.setPhoneNumber(user.getPhoneNumber());
+        newUser.setUserRole(user.getUserRole());
+    }
+
+
 }
