d030f5d9ba1c9286a304e5387ca742c4e2ecb56a
Sachin302cse <sachin.19cs302@citranchi.ac.in>
2025-02-27 17:41:38 +0530

    adding utility

--- src/main/java/com/example/dine_in_order_api/utility/ReponseStructure.java
+++ src/main/java/com/example/dine_in_order_api/utility/ReponseStructure.java
@@ -1,4 +1,28 @@
 package com.example.dine_in_order_api.utility;
 
-public class ReponseStructure {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReponseStructure<T> {
+
+    private int status;
+    private String message;
+    private T data;
+
+    public static <T> ReponseStructure<T> create(HttpStatus status, String message, T data){
+        ReponseStructure<T> reponse = new ReponseStructure();
+        reponse.status= status.value();
+        reponse.message=message;
+        reponse.data=data;
+
+        return reponse;
+    }
+
 }
